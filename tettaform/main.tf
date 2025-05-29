801cff40fef8d6a49f85180b43c2eb31ecc27556
sudhabec <113691987+sudhabec@users.noreply.github.com>
2025-05-30 00:16:18 +0530

    Update main.tf

--- tettaform/main.tf
+++ tettaform/main.tf
@@ -57,6 +57,7 @@ resource "aws_instance" "web" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
+  key_name = "awsdevops" 
 
   user_data = <<-EOF
               #!/bin/bash
