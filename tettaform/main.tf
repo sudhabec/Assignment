72202ce0ddc07ff5f23a9715f543cd861b2f3f86
sudhabec <113691987+sudhabec@users.noreply.github.com>
2025-05-30 00:33:47 +0530

    Update main.tf

--- tettaform/main.tf
+++ tettaform/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  ami                         = "ami-04568bde0d686e039" # Amazon Linux 2 (us-east-1)
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
